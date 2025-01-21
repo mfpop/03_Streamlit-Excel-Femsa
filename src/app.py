a667b76b55a2a6b314094144bfa0644e851f796b
mihai <mihaipop@outlook.com>
2025-01-21 12:05:09 -0800

    d

--- src/app.py
+++ src/app.py
@@ -1,31 +1,21 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+from streamlit_option_menu import option_menu
 
-# Title of the app
-st.title("Maxon Dashboard")
 
-# Upload the Excel file
-uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+st.title("Maxon Dashboards")
 
-if uploaded_file:
-    # Read the Excel file
-    df = pd.read_excel(uploaded_file)
+# with st.sidebar:
+selected = option_menu(
+    menu_title=None,
+    options=["Home", "Dashboards", "Contact"],
+    icons=["house", "book", "envelope"],
+    default_index=0,
+    orientation="horizontal",
+)
 
-    # Display the dataframe
-    st.write(df)
-
-    # Select columns for the x and y axes
-    x_axis = st.selectbox("Select the column for the x-axis", df.columns)
-    y_axis = st.selectbox("Select the column for the y-axis", df.columns)
-
-    # Generate the plot
-    fig, ax = plt.subplots()
-    ax.plot(df[x_axis], df[y_axis])
-    ax.set_xlabel(x_axis)
-    ax.set_ylabel(y_axis)
-    ax.set_title(f"{y_axis} vs {x_axis}")
-
-    # Display the plot
-    st.pyplot(fig)
-st.write("vector-lmd")
+if selected == "Home":
+    st.write(f"Welcome to the Maxon")
+if selected == "Dashboards":
+    st.write(f"Dashboars")
+if selected == "Contact":
+    st.write(f"Contact us")
---
+++ src/test.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Title of the app
+st.title("Maxon Dashboard")
+
+# Upload the Excel file
+uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+
+if uploaded_file:
+    # Read the Excel file
+    df = pd.read_excel(uploaded_file)
+
+    # Display the dataframe
+    st.write(df)
+
+    # Select columns for the x and y axes
+    x_axis = st.selectbox("Select the column for the x-axis", df.columns)
+    y_axis = st.selectbox("Select the column for the y-axis", df.columns)
+
+    # Generate the plot
+    fig, ax = plt.subplots()
+    ax.plot(df[x_axis], df[y_axis])
+    ax.set_xlabel(x_axis)
+    ax.set_ylabel(y_axis)
+    ax.set_title(f"{y_axis} vs {x_axis}")
+
+    # Display the plot
+    st.pyplot(fig)
+st.write("vector-lmd")
